27f2034af7319b6120870d7e5e288422f6c8606a
Wemerson <wemersoncontas22@gmail.com>
2024-02-14 17:36:05 -0300

    exercicio-cond-pt01

---
+++ 02-EstruturaCondicional/uri1035/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace uri1035;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B, C, D;
+        Console.WriteLine("Informe os valores A, B, C e D: ");
+        string[] valores = Console.ReadLine().Split(' ');
+        A = int.Parse(valores[0]);
+        B = int.Parse(valores[1]);
+        C = int.Parse(valores[2]);
+        D = int.Parse(valores[3]);
+
+        if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0){
+            Console.WriteLine("Valores aceitos");
+        } else {
+            Console.WriteLine("Valores nao aceitos");
+        }
+
+    }
+}
